ca6875a26500e8c8c9084b27935b8008209b0956
0xRickJames <0xrickjames@gmail.com>
2024-08-06 12:22:03 -0400

    selling error

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -121,7 +121,7 @@ const Contact = () => {
                         value={formData.message}
                         onChange={handleChange}
                         rows={5}
-                        placeholder="What can we discpose for you?"
+                        placeholder="What can we dispose for you?"
                         className="border-stroke dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary"
                       ></textarea>
                     </div>
